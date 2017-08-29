632aaede7f2abc40e06662aa7bbc41109da811ef
unknown <reach.varun.ms@gmail.com>
2017-08-29 12:33:56 -0600

    Final commit on local laptop. Moving work to personal laptop"

--- MonsterChase/Player.h
+++ MonsterChase/Player.h
@@ -5,4 +5,14 @@ class Player
 	public:
 		//Player Constructor
 		Player();
+		
+		//Function that Moves that Moves Character
+		int Move(char dir);
+
+		//Returns X Position of the Character
+		int PosX();
+
+		//Returns Y Position of the Character
+		int PosY();
 };
+
